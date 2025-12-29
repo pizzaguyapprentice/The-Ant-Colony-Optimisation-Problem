fd8cec57c90233879606459d22531d98cefc81ba
Microsoft-Hater <B00164190@mytubublin.ie>
2025-12-29 14:31:02 +0000

    Updated Main To Calculate The Amount Of Pheromones To Deposit On A Path

--- src/main/java/ant/Main.java
+++ src/main/java/ant/Main.java
@@ -5,16 +5,22 @@ public class Main{
 	public static final int distanceImportance = 2;
 	public static final int pheromoneImportance = 2;
 	public static final double dissipationRate = 0.2;
-	public static final double pheromoneRate = 0.4;
+	public static final double pheromoneRate = 2;
+	public static final int GENS = 1;
 	public static void main(String[] args) throws IOException{
 		Ant ant = new Ant(World.createWorld());
 
-		for(int i = 0; i < 10; i++){
+		for(int i = 0; i < GENS; i++){
 			while (!ant.nextAction());
 			System.out.printf("Finished Iteration %d\n", i);
 			Edge[] edgesTraversed = ant.getEdgesTraversed();
-			for(Edge edge : edgesTraversed) {
-				edge.addPheromone(pheromoneRate);
+			for(Edge edge : edgesTraversed){
+				double totalDistance = 0;
+				for(Edge edge2 : edgesTraversed){
+					totalDistance =+ edge2.getDistance();
+				}
+				double pheromones = pheromoneRate / totalDistance;
+				edge.addPheromone(pheromones);
 				System.out.println("Edge Traversed: " + edge.getName());
 			}
 			World.dissipatePheromone(dissipationRate);
