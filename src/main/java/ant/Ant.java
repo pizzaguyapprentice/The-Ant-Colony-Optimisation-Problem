bfae5e72d341b4a33ef2fe992c27a1159d65b9ce
pizzaguyapprentice <matisew8247@gmail.com>
2025-12-29 14:16:02 +0000

    Seperated functionality in nextAction into seperate working functions, cleaner code and more visible of each part at work.

--- src/main/java/ant/Ant.java
+++ src/main/java/ant/Ant.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Ant {
-    // private HomeNode homeNode;
     private Node position;
     private Node lastPosition;
     private boolean collectedFood = false;
@@ -48,23 +47,46 @@ public class Ant {
         return edgesTraversed.toArray(new Edge[0]);
     }
 
-    boolean nextAction() throws FileNotFoundException {
-        // Distance holder stores full values for the distances of each path e.g 10 , 15 , 20
-        // These values will then be used to calculate the visibility metrics for each path
-        // Position is a local variable that holds the current node the ant is on
-        // Reads from position.getNeighbours() to acquire the paths, its edges and their distances
+    public boolean nextAction() throws FileNotFoundException {
+        // distanceHolder stores full values for the distances of each path e.g 10 , 15 , 20
+        // pheromoneHolder stores full values of the total pheromone of each path.
+        // These values will then be used to calculate the visibility metrics for each path in the VisibilityArray
+
         ArrayList<Double> distanceHolder = new ArrayList<Double>(0);
 		ArrayList<Double> pheromoneHolder = new ArrayList<Double>(0);
+        ArrayList<Double> visibilityArray = new ArrayList<Double>();
+        ArrayList<Double> pathProbabilityArray = new ArrayList<Double>(0);
+        double visibilityTotal = 0;
+
+		System.out.println();
+
+        setParameters(distanceHolder,pheromoneHolder);
+
+        System.out.println();
+
+        visibilityTotal = setVisibility(distanceHolder, pheromoneHolder, visibilityArray, visibilityTotal);
+
+		System.out.println();
+
+        calculateProbablePaths(visibilityArray, visibilityTotal, pathProbabilityArray);
+
+		System.out.println();
+
+        return chooseNextPath(pathProbabilityArray);
+
+    }
+
+    public void setParameters(ArrayList<Double> distanceHolder, ArrayList<Double> pheromoneHolder){
         for(int i = 0; i < position.getNeighbours().length; i++){
             distanceHolder.add(position.getNeighbour(i).getEdge().getDistance());
 			pheromoneHolder.add(position.getNeighbour(i).getEdge().getPheromone());
         }
 
 		System.out.println();
-
-        double visibilityTotal = 0;
-        ArrayList<Double> visibilityArray = new ArrayList<>();
-        // path visibility without pheromone influence
+        
+    }
+    public double setVisibility(ArrayList<Double> distanceHolder, ArrayList<Double> pheromoneHolder, ArrayList<Double> visibilityArray, double visibilityTotal){
+        
         for(int i = 0; i < distanceHolder.size(); i++){
             double heuristic = 1/distanceHolder.get(i);
 			heuristic = Math.pow(heuristic, Main.distanceImportance);
@@ -75,14 +97,14 @@ public class Ant {
 			double visibility = pheromone * heuristic;
             visibilityTotal += visibility;
             visibilityArray.add(visibility);
-
             System.out.println("Visibility: " + visibility);
         }
 
-		System.out.println();
+        return visibilityTotal;
+    }
 
-		double probabilityTotal = 0;
-		ArrayList<Double> pathProbabilityArray = new ArrayList<>(0);
+    public void calculateProbablePaths(ArrayList<Double> visibilityArray, double visibilityTotal, ArrayList<Double> pathProbabilityArray){
+        double probabilityTotal = 0;
 		for (int i = 0; i < visibilityArray.size(); i++) {
 			double visibility = visibilityArray.get(i);
 			double probability = visibility/visibilityTotal;
@@ -91,33 +113,16 @@ public class Ant {
 			probabilityTotal += probability;
 			System.out.println("Path Probability: " + probability);
 		}
+    }
 
-		System.out.println();
+    public Boolean chooseNextPath(ArrayList<Double> pathProbabilityArray){
 
-        // Using BigDecimal for precision when rounding the temp
-        BigDecimal rounding = BigDecimal.valueOf(probabilityTotal);
-        rounding = rounding.setScale(16, RoundingMode.HALF_UP);
-
-        System.out.println("Rounded number: " + rounding);
-        
-        if(rounding.compareTo(BigDecimal.ONE) == 0){
-            System.out.println("Rounded number equals 1");
-        }
-        else{
-            System.out.println("Rounded number does not equal 1");
-        }
-        // Decision made based on the rolled number and the visibility metrics
-
-		// Generate a random double from 0.0 to 1.0
-		double randomNumber = new Random().nextDouble();
+        double randomNumber = new Random().nextDouble();
 
         double incrementedDecision = 0;
         System.out.println("The decision: "+ randomNumber);
 
         for(int i = 0; i < position.getNeighbours().length; i++){
-            // testing temp variable
-            // double tempcheck = 0.99;
-            // decide if the roll is less than or equal to the incremented decision
             incrementedDecision += pathProbabilityArray.get(i);
             if(randomNumber <= incrementedDecision){
                 System.out.println("You got it!");
@@ -142,7 +147,8 @@ public class Ant {
                 System.out.println("You did NOT get it...");
             }
         }
-		System.out.println("Colected Food: " + collectedFood);
+        System.out.println("Colected Food: " + collectedFood);
 		return false;
+
     }
 }
