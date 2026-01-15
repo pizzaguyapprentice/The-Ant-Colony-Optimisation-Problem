0bb28a9ba0bb28695c7913969f385206c7263555
Microsoft-Hater <B00164190@mytubublin.ie>
2026-01-15 02:22:29 +0000

    Updated World To Not Be Used In A Static Context, Ant Now Only Uses A Hashmap To Store Edges Traversed, Main Now Calculates Total Distance While Ant Is Traversing

--- src/main/java/ant/Main.java
+++ src/main/java/ant/Main.java
@@ -5,15 +5,17 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 
 public class Main{
-	public static final double distanceImportance = 0.5;
+	public static final double distanceImportance = 0.2;
 	public static final double pheromoneImportance = 2;
 	public static final double dissipationRate = 0.7;
-	public static final double pheromoneRate = 3;
+	public static final double pheromoneRate = 5;
+
 	// Number Of Ants Per Generation
 	public static final int NUM_ANTS = 100;
-	public static final int GENS = 50;
+	public static final int GENS = 100;
 	public static void main(String[] args) throws IOException{
-		Ant ant = new Ant(World.createWorld());
+		World world = new World();
+		Ant ant = new Ant(world.getStartNode());
 
 		File file = new File("src/main/resources/" + System.currentTimeMillis() + ".txt");
 		file.createNewFile();
@@ -23,16 +25,14 @@ public class Main{
 			HashMap<String, Double> totalPheromoneMap = new HashMap<String, Double>();
 
 			for(int j = 0; j < NUM_ANTS; j++){
-				while(!ant.nextAction());
+				double totalDistance = 0;
+				while(!ant.nextAction()){
+					totalDistance = totalDistance + ant.lastEdge.getDistance();
+				}
+				totalDistance = totalDistance + ant.lastEdge.getDistance();
+				System.out.println(totalDistance);
 
 				HashMap<String, Edge> edgesTraversed = ant.getEdgesTraversed();
-
-				double totalDistance = 0;
-				for(Edge secondEdgeName : ant.edgesTraversed2){
-					totalDistance = totalDistance + secondEdgeName.getDistance();
-				}
-
-				System.out.println(totalDistance);
 
 				for(String edgeName : edgesTraversed.keySet()){
 					double pheromones = pheromoneRate / totalDistance;
@@ -42,26 +42,23 @@ public class Main{
 					else{
 						totalPheromoneMap.put(edgeName, pheromones);
 					}
-					// System.out.printf("Edge Traversed: %s  Pheromones Deposited: %.5f\n", edgeName, pheromones);
 				}
-
-				ant.edgesTraversed2.clear();
 				ant.setLastPosition(null);
 				ant.resetEdgesTraversed();
 				ant.setCollectedFood(false);
 			}
 			System.out.printf("Finished Iteration %d\n", i);
 			
-			World.dissipatePheromone(dissipationRate);
+			world.dissipatePheromone(dissipationRate);
 
 			for(String edgeName : totalPheromoneMap.keySet()){
-				World.updateEdgePheromone(edgeName, totalPheromoneMap.get(edgeName));
+				world.updateEdgePheromone(edgeName, totalPheromoneMap.get(edgeName));
 			}
 
-			World.printEdgePheromone();
+			world.printEdgePheromone();
 
 			pw.println("Gen " + i);
-			World.outputEdgePheromone(pw);
+			world.outputEdgePheromone(pw);
 
 			
 			// double totalDistance = 0;
